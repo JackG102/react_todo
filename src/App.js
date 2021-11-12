b3f15c17c1251f20921d2044ab476d9686263fbd
Jack Garratt <jack.garratt@icfnext.com>
2021-11-12 06:42:54 -0500

    Added comment about state naming between message and messages

--- src/App.js
+++ src/App.js
@@ -3,6 +3,17 @@ import NoteForm from './components/NoteForm';
 import NoteList from './components/NoteList';
 
 const App = () => {
+  // The difference 'message' and 'messages' in this app is this:
+  // 'message' in NoteForm is used to help with keeping track of the
+  // original value, so we can update the messages array that holds
+  // all the values.  Just know that the 'message' (singular) state 
+  // is really only used to help with Update functionality of the TODO app.
+  
+  // Also note: in the NoteList component there is some poor naming with the 
+  // map that uses messages as the argument. 
+  
+  // Naming is hard and this is my first
+  // go through in a fully functioning app on my own.
   const [messages, setMessages] = useState([
     'Take dog out.',
     'Clean up bedroom.',
