e5c14596f9e669e1170b8f4f14773a20f15ed18f
Jack Garratt <jack.garratt@icfnext.com>
2021-11-11 20:10:55 -0500

    Added comment on Filter function

--- src/components/Note.js
+++ src/components/Note.js
@@ -2,7 +2,13 @@ import React from 'react';
 import './Note.css';
 
 const Note = ({message, messages, setMessages}) => {
+  // Note to Self: Filter is a pain to learn initially, but it makes sense.
+  // Filter is basically looping(foreach) through an array by item.
+  // Then, doing some logic it creates a new array filtering out
+  // items that failed the logic test.
 
+  // In this instance, we want to add back every message
+  // except the message clicked on.
   const deleteMessage = () => {
     setMessages(messages.filter((item) => item !== message ));
   };
